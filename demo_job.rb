8e7ff1a257245f9592f0f2aee1690212d3eedddd
Woffendin, Michael Harrison <michael.woffendin@oregonstate.edu>
2015-04-22 12:55:38 -0700

    Remove trailing whitespace

--- demo_job.rb
+++ demo_job.rb
@@ -2,7 +2,7 @@ require "json"
 
 # All the example tiles use the same data
 # Note that the first nested array ["Person", "Tickets"] denotes the data column names used
-#   internally by Google Charts. You must include this or weird stuff will happen. 
+#   internally by Google Charts. You must include this or weird stuff will happen.
 data = [["Person", "Tickets"], ["Bob", 52], ["Frank", 83], ["Jane", 57], ["Mary", 60]]
 
 # Notice the .to_json called on the data. This is important!
